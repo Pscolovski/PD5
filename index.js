258df9057abf94a42effedbae32298e44aa8ac6a
Pscolovski <157747460+Pscolovski@users.noreply.github.com>
2024-04-11 11:22:25 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let number = 12;
+console.log(typeof number);
+
+let bigintNumber = BigInt(12345678901234567890);
+console.log(typeof bigintNumber);
+
+let text = 'Beautiful JavaScript World';
+console.log(typeof text);
+
+let isTrue = (true == false);
+console.log(typeof isTrue);
+
+let undefinedVariable;
+console.log(typeof undefinedVariable);
+
+let nullVariable = null;
+console.log(typeof nullVariable);
+
+let obj = { key: 'value' };
+console.log(typeof obj);
+
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+  humanName: undefined,
+  humanAge: null
+};
+
+console.log(typeof otherNumber);
+console.log(typeof textOne);
+console.log(typeof textTwo);
+console.log(typeof textThree);
+console.log(typeof isEqual);
+console.log(typeof petName);
+console.log(typeof petAge);
+console.log(typeof human);
